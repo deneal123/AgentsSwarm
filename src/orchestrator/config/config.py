5090e6a9b10db874eff44846622ea97e88255e08
deneal123 <dfvolkhin@edu.hse.ru>
2026-03-21 13:43:49 +0000

    v0.0.0
    init

---
+++ src/orchestrator/config/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from dynaconf import Dynaconf
+
+BASE_DIR = Path(__file__).parent
+
+settings = Dynaconf(
+    root_path=BASE_DIR,
+    environments=True,
+    load_dotenv=True,
+    envvar_prefix=False,
+    settings_files=["settings.toml"],
+)
