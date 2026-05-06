63208040b7cd1de3cd6c34eb75e9019f7c727f16
Вольхин Данил Федорович <dfvolkhin@gmail.com>
2026-05-06 13:04:15 +0300

    frontend: document layers and enforce feature public imports

--- frontend/src/features/auth/components/index.js
+++ frontend/src/features/auth/components/index.js
@@ -4,3 +4,4 @@ export { default as PasswordStrength } from "./PasswordStrength";
 export { default as AuthFormCard } from "./AuthFormCard";
 export { default as AuthPageShell } from "./AuthPageShell";
 export { default as AuthPageHeader } from "./AuthPageHeader";
+export { default as AuthModal, useAuthModal } from "./AuthModal";
---
+++ frontend/src/features/chat/index.js
@@ -0,0 +1,2 @@
+export { default as ChatPageContainer } from "./page/ChatPageContainer";
+export { default as MessageRenderer } from "./components/MessageRenderer";
